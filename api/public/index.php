32b50f62c8c1b345e83a1dc49607d8a8b4eb8257
Αλέξανδρος Μυρωνιδης <alxmrd@Alexandross-MacBook-Air.local>
2018-06-09 17:01:47 +0300

    api work successfully

--- api/public/index.php
+++ api/public/index.php
@@ -22,7 +22,6 @@ require __DIR__ . '/../src/dependencies.php';
 
 // Register middleware
 require __DIR__ . '/../src/middleware.php';
-
 // Register routes
 require __DIR__ . '/../src/routes.php';
 
