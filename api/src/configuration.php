3146c0ef2bac88cc04cb5755ba2a01036abb5b8c
Αλέξανδρος Μυρωνιδης <alxmrd@Alexandross-MacBook-Air.local>
2018-06-14 19:47:18 +0300

    add db settings to conf file

--- api/src/configuration.php
+++ api/src/configuration.php
@@ -1,4 +1,20 @@
 <?php
 /*********DB Settings**************/
 
+$user = 'root';
+$password = 'root';
+$db = 'DiplomaThesis';
+$host = '127.0.0.1';
+$port = 3306;
+
+$mysqli = new mysqli($host,  $user, $password, $db, $port);
+$mysqli->set_charset("utf8");
+if (!$mysqli) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
+
+
 /*********DB Settings**************/
--- api/src/routes.php
+++ api/src/routes.php
@@ -13,22 +13,9 @@ $app->get('/hello/{name}', function (Request $request, Response $response, array
 });
 
 $app->get('/volunteers', function (Request $request, Response $response, array $args) {
-    $user = 'root';
-$password = 'root';
-$db = 'DiplomaThesis';
-$host = '127.0.0.1';
-$port = 3306;
-
-$mysqli = new mysqli($host,  $user, $password, $db, $port);
-$mysqli->set_charset("utf8");
-if (!$mysqli) {
-    echo "Error: Unable to connect to MySQL." . PHP_EOL;
-    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-    exit;
-}
-
-        $arr = array();
+    require_once ('configuration.php');
+    
+       $arr = array();
 		$response = array([]);
 		$query = "SELECT * FROM volunteer";
 		$result = $mysqli->query($query);
