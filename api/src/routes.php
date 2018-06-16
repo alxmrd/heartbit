5a74f0ef692fbcd25d1b2c0393bc67b913297e21
Αλέξανδρος Μυρωνιδης <alxmrd@Alexandross-MacBook-Air.local>
2018-06-16 13:08:18 +0300

    Endpoints that return all the volunteers in the database and a specific one #1

--- api/src/routes.php
+++ api/src/routes.php
@@ -12,12 +12,29 @@ $app->get('/hello/{name}', function (Request $request, Response $response, array
     return $response;
 });
 
-$app->get('/volunteers', function (Request $request, Response $response, array $args) {
+$app->get('/api/volunteers', function (Request $request, Response $response, array $args) {
     require_once ('configuration.php');
     
        $arr = array();
 		$response = array([]);
 		$query = "SELECT * FROM volunteer";
+		$result = $mysqli->query($query);
+		$rows = array();
+         while($r = mysqli_fetch_assoc($result)) {
+                 $rows[] = $r;
+        }
+        print json_encode($rows);
+
+mysqli_close($mysqli);
+});
+
+$app->get('/api/volunteers/{id}', function (Request $request, Response $response, array $args) {
+     require_once ('configuration.php');
+    $volunteers_id =(int)$args['id'];
+    
+       $arr = array();
+		$response = array([]);
+		$query = "SELECT * FROM volunteer WHERE id=$volunteers_id";
 		$result = $mysqli->query($query);
 		$rows = array();
          while($r = mysqli_fetch_assoc($result)) {
