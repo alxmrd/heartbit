4355f294c3ce4108962c8960394b0261010c85ad
Αλέξανδρος Μυρωνιδης <alxmrd@Alexandross-MacBook-Air.local>
2018-09-16 19:12:11 +0300

    post method worked succesfully, error retrieving user data

--- api/src/routes.php
+++ api/src/routes.php
@@ -1,4 +1,6 @@
 <?php
+//include("configuration.php");
+
 
 use Slim\Http\Request;
 use Slim\Http\Response;
@@ -45,24 +47,28 @@ $app->get('/api/volunteers/{id}', function (Request $request, Response $response
 mysqli_close($mysqli);
 });
 
-$app->post('/login', function (Request $request, Response $response, array $args) {
+$app->post('/api/login', function (Request $request, Response $response, array $args) {
         require_once ('configuration.php');
-       $volunteers_id =(int)$args['id'];
-       $username = mysql_real_escape_string($_POST['username']);
-       $password = mysql_real_escape_string($_POST['password']);
+        session_start();
+       //$volunteers_id =(int)$args['id'];
+       $username = mysqli_real_escape_string($mysqli,$_POST['username']);
+       $password = mysqli_real_escape_string($mysqli,$_POST['password']);
        
           $arr = array();
                    $response = array([]);
-                   $query = "SELECT * FROM volunteer WHERE username='$username' AND password='password' ";
-                   $result = $mysqli->query($query);
-                   if (mysqpli_num_rows($reslut) == 1) {
+                   $query = "SELECT id FROM volunteer WHERE username='$username' AND password='$password'";
+                   $result = mysqli_query($mysqli,$query);
+                   $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+                   $active = $row['active'];
+
+                   $count = mysqli_num_rows($result);
+                   if (mysqli_num_rows($result) == 1) {
 
                         $_SESSION['username'] = $username;
                         $_SESSION['success'] = "You are now logged in";
-                        header('logation: routes.php');
+                       
                 }else{
-                        array_push($errors, "wrong username/password combination  ");
-                        header('location: login.php');
+                        echo "Wrong username & password compination";
                 }
                    $rows = array();
             while($r = mysqli_fetch_assoc($result)) {
