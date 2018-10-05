7e660875522e98eccdc5a1996aa00adbca8f1592
Αλέξανδρος Μυρωνιδης <alxmrd@Alexandross-MacBook-Air.local>
2018-10-05 16:29:40 +0300

    haedcoded table data into volunteers page using our volunteers data

--- src/containers/Volunteers.js
+++ src/containers/Volunteers.js
@@ -7,31 +7,51 @@ export default class Volunteer extends Component {
     <Table striped bordered condensed hover>
   <thead>
     <tr>
-      <th>#</th>
-      <th>First Name</th>
-      <th>Last Name</th>
-      <th>Username</th>
+      <th>id</th>
+      <th>username</th>
+      <th>password</th>
+      <th>tel1</th>
+      <th>tel2</th>
+      <th>name</th>
+      <th>surname</th>
+      <th>email</th>
+      <th>notes</th>
+      <th>latesttraining</th>
+      <th>dateofbirth</th>
+      <th>address</th>
+      
     </tr>
   </thead>
   <tbody>
     <tr>
       <td>1</td>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
+      <td>alex</td>
+      <td>test</td>
+      <td>35235</td>
+      <td>25535</td>
+      <td> alex</td>
+      <td>alex</td>
+      <td> alexmrd1@hotmail.com</td>
+      <td>dgadg</td>
+      <td>2018-05-14</td>
+      <td>2017-05-09</td>
+      <td>sbsdbsdba</td>
     </tr>
     <tr>
-      <td>2</td>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
+    <td>2</td>
+      <td>joey</td>
+      <td>1234</td>
+      <td>696969</td>
+      <td> null</td>
+      <td>joey</td>
+      <td> tribbiani</td>
+      <td>asasas@gmail.com</td>
+      <td>null</td>
+      <td>2018-06-04</td>
+      <td>2018-06-28</td>
+      <td>asdfadsgad</td>
     </tr>
-    <tr>
-      <td>3</td>
-      <td colSpan="2">Larry the Bird</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
+   </tbody>
 </Table>
   );
   }
