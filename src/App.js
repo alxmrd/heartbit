dbc7f5295d5ae24ece0d1172cb273c7774ac5fe9
Αλέξανδρος Μυρωνιδης <alxmrd@Alexandross-MacBook-Air.local>
2018-10-11 18:49:24 +0300

    material ui worked

--- src/App.js
+++ src/App.js
@@ -1,31 +1,51 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import { Nav, Navbar } from "react-bootstrap";
-import RouteNavItem from "./components/RouteNavItem";
-import "./App.css";
-import Routes from "./Routes";
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App container">
-        <Navbar fluid collapseOnSelect>
-          <Navbar.Header>
-            <Navbar.Brand>
-              <Link to="/">HeartBit</Link>
-            </Navbar.Brand>
-            <Navbar.Toggle />
-          </Navbar.Header>
-          <Navbar.Collapse>
-            <Nav pullRight>
-              <RouteNavItem href="/signup">Home</RouteNavItem>
-              <RouteNavItem href="/login">Login</RouteNavItem>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-        <Routes />
-      </div>
-    );
+const styles = {
+  root: {
+    flexGrow: 1
+  },
+  grow: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20
   }
+};
+
+function ButtonAppBar(props) {
+  const { classes } = props;
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="Menu"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" color="inherit" className={classes.grow}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
-export default App;
+
+ButtonAppBar.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(ButtonAppBar);
